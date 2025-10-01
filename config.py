71026308d1c82e80376b16e1fb254321ef6f7005
munna124567890op-cyber <munna124567890op@gmail.com>
2025-10-01 09:59:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ API_HASH = "8d70e5e932f9f30d5af8ad39c6b8ece5"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = "8421110576"
-SUPPORT_GRP = "https://t.me/+9Snnx3cWVLJiYTU1"
+SUPPORT_GRP = "+9Snnx3cWVLJiYTU1"
 UPDATE_CHNL = "LOVE_BFF_PFP"
 OWNER_USERNAME = "i_t_z_broken"
